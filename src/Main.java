b28ffdd8f78769703990b3efa283931a8bfd53d9
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-04-02 21:39:19 +0300

    add try and catch

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,23 @@ public class Main {
     public static void main(String[] args) {
         int [] nums = new int[5];
         try {
-            nums[10] = 5;
+            //block of code to monitor for errors
+            System.out.println("In try 1");
+            errorMethod(nums);
+            System.out.println("In try 2");
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("In catch 1");
         }
         catch (Exception e) {
+            //handler for Exception
+            System.out.println("In catch 2");
 
         }
+        System.out.println("Rest of program");
+    }
+
+    public  static  void errorMethod(int [] nums){
+        nums[10] = 25;
 
     }
 }

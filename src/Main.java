fd0ab9bcb2cb48647977b264b56f65e816e69c8a
Kipleting Edwin <kipletingedwin4@gmail.com>
2024-04-02 21:28:34 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int [] nums = new int[5];
+        try {
+            nums[10] = 5;
+        }
+        catch (Exception e) {
+
+        }
+
+    }
+}
